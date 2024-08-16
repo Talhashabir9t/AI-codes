728ee1e447aabe22a4d1d27fad56cb65d27fcf06
Talhashabir9 <mmmvvv1112222@gmail.com>
2024-08-16 00:24:28 -0700

    first commit

---
+++ ist.py
@@ -0,0 +1,9 @@
+myname="talha"
+markes = 77
+
+if(markes==90):
+    print("talha is excelent / grade A")
+elif(markes<90):
+    print("talha is good / grade B")
+else:
+    print("talha is failed / grade F")
---
+++ secend.py
@@ -0,0 +1,14 @@
+num1=input('enter your age; ' )
+num1=int(num1)
+num2=input('enter your height; ' )
+num2=int(num2)
+num3=input('enter your education; ' )
+num3=int(num3)
+# now print all inputs 
+print(num1,num2,num3)
+if(num1==18 and num2==5 and num3==14):
+if(num1 or num2 ):
+ if(num2 or num3):
+if (num1 or num3):
+   print("YOU HAVE PASSED")
+else:("you have failed")
